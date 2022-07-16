3ef3f556455bf344672c6b05ee262a2676c7156c
Katye Stevens <katye333@gmail.com>
2022-07-16 13:13:21 -0400

    feat: Allow drawing on the app screen

--- app/src/main/java/com/katye333/drawit/DrawingView.kt
+++ app/src/main/java/com/katye333/drawit/DrawingView.kt
@@ -3,6 +3,7 @@ package com.katye333.drawit
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 // Used as a View -- Not Activity
@@ -32,6 +33,56 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
 
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
         mBrushSize = 20.toFloat()
+    }
+
+    // Extending the View class
+    // Called once the view is inflated
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)      // Uses rgba - 256 possible values
+        canvas = Canvas(mCanvasBitmap!!)    // Nullable so not-null assertion must be used
+    }
+
+    // Change Canvas back to Canvas? if it fails
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        // Start drawing at top left corner
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        // Events allowed: Up, Move, Down
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                mDrawPath!!.moveTo(touchX!!, touchY!!)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                mDrawPath!!.lineTo(touchX!!, touchY!!)
+            }
+            MotionEvent.ACTION_UP -> {
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+            else -> return false
+        }
+
+        invalidate()
+        return true
     }
 
     internal inner class CustomPath(var color: Int,
